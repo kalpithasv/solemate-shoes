38b46f2f3151e43deb9785047f8df62ea918bd8a
Avanimaletha <avanimaletha@gmail.com>
2023-04-22 22:22:47 +0530

    initialize ProductCart file

--- src/pages/Shop.js
+++ src/pages/Shop.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import ProductCart from '../components/ProductCart';
 
 const Shop = () => {
   return (
     <div className="text-black text-xl">
       <h1>Shop</h1>
+      <ProductCart/>
     </div>
   );
 };
