477ff1e47c598db345d0f72d9befc85e31a228ef
kalpithasv <kalpithasv@gmail.com>
2023-04-27 14:00:24 +0530

    :)

--- src/components/Prod.jsx
+++ src/components/Prod.jsx
@@ -12,14 +12,6 @@ export default function CardComponent() {
                 <h4 className="text-xl font-semibold tracking-tight text-blue-600">
                     Nike Jordan Shoes
                 </h4>
-                <p className="mb-2 leading-normal">
-                Featuring a panther-paw-like sole and "cat eye" holograph, Michael rode the mid-top shoe all the way to his 6th ring in '98,
-                the last triumph of his legendary career. The Air Jordan 13 Retro brings back the stealthy,
-                shrouded design with a fresh mix of colours and textures.
-                </p>
-                <button className="px-4 py-2 text-sm text-blue-100 bg-blue-500 rounded shadow">
-                    Add to Cart
-                </button>
             </div>
         </div>
     );
